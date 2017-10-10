74b6cdea6f9139f5bb6fe4cdeab511ccd400965c
Wade Gong <wadetgong@gmail.com>
2017-10-10 14:03:14 -0500

    adds doge message on hide

--- content.js
+++ content.js
@@ -1,5 +1,48 @@
 /* global chrome */
 
+const chooseRandom = arr => {
+  const randomIndex = Math.floor(Math.random() * arr.length)
+  return arr[randomIndex]
+}
+
+const getMessage = () => {
+  const messages = [
+    'Much goodbye',
+    'No thank u',
+    'Next pls',
+    'Wow',
+    'No like',
+    'Much next',
+  ]
+  return chooseRandom(messages)
+}
+
+const getRandomInRange = (min, max) => {
+  return Math.round((Math.random() * (max - min) + min) * 100)
+}
+
+const getRandomColor = () => {
+  const colors = [
+    '#0500fe',
+    '#ff00fe',
+    '#fe0400',
+    '#fe7e00',
+    '#0f0',
+  ]
+  return chooseRandom(colors)
+}
+
+const getDogeMessage = () => {
+  let message = document.createElement('p')
+  message.style.left = `${getRandomInRange(0.1, 0.6)}%`
+  message.style.top = `${getRandomInRange(0.0, 0.45)}%`
+  message.style.color = getRandomColor()
+  message.style.transform = `translateY(0em) rotate(${getRandomInRange(-0.12, 0.12)}deg)`
+  message.innerHTML = getMessage()
+  message.classList.add('doge-msg')
+  return message
+}
+
 const traversePosts = (domElements, hiddenPosts) => {
   for (let i = 0; i < domElements.length; i++) {
     let currentElem = domElements[i]
@@ -12,6 +55,7 @@ const traversePosts = (domElements, hiddenPosts) => {
       let jobInfo = currentElem.querySelector('.job-title a')
       let hrefString = jobInfo.href
       let jobTitle = jobInfo.text
+      let host = jobInfo.hostname
       let company = currentElem
         .querySelector('.job-company a')
         .text
@@ -20,15 +64,23 @@ const traversePosts = (domElements, hiddenPosts) => {
         // If job post matches a hidden post, hide the post
         currentElem.setAttribute('id', 'jobdoge-hidden-post')
       } else {
+
+        // Set up doge message
+        let dogeMsg = getDogeMessage()
+        currentElem.prepend(dogeMsg)
+
         // Set up button
         var button = document.createElement('Button')
         button.innerHTML = 'Hide'
         button.classList.add('jobdoge-remove')
         button.addEventListener('click', function() {
+          // Update CSS to enable transition
           currentElem.setAttribute('id', 'jobdoge-hidden-post')
+          dogeMsg.classList.add('doge-msg-hidden')
 
+          // Save relevant data to storage
           let data = {}
-          data[hrefString] = {jobTitle, company}
+          data[hrefString] = {jobTitle, company, host}
           chrome.storage.sync.set(data);
         });
         currentElem.prepend(button)
